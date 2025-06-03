a2cdb65da669669db75e43f6f0e2d7cf52a561dc
saugat1070 <saugatgiri1070@gmail.com>
2025-06-03 16:00:29 +0545

    adding delete function

--- src/todo/todo.Controller.ts
+++ src/todo/todo.Controller.ts
@@ -9,27 +9,51 @@ export class TodoController{
         console.log("I am in todo Controller")
       this.io.on("connection",(socket:Socket)=>{
         console.log("new client connected");
-        socket.on("addTodo",(data)=> this.handleAddTodo(socket,data))
+        socket.on("addTodo",(data)=> this.handleAddTodo(socket,data));
+        socket.on("deleteTodo",(data)=>this.handleTodoDelete(socket,data))
       })  
     }
 
     private async handleAddTodo(socket:Socket,data:any){
-        console.log(data)
         const {task,deadLine,status} = data
         try {
-            const createTodo = await todoModel.create({
+            await todoModel.create({
             task:task,
             dealLine:deadLine,
             status:status
         })
+
+        const todoList = await todoModel.find()
         socket.emit("todoResponse",{
             status:"success",
-            data : createTodo
+            data : todoList
         })
         } catch (error) {
-            console.log(error)
+            socket.emit("todo_response",{
+                status: "error",
+                error
+            })
         }
 
         
+    }
+
+    private async handleTodoDelete(socket:Socket,data:{id:string}){
+        const {id} = data
+        const deleteTodo = await todoModel.findByIdAndDelete(id);
+        if(!deleteTodo){
+            socket.emit("todoResponse",{
+                status: "failed",
+                message : "Todo is not found"
+            })
+            return;
+        }
+        const todoList = await todoModel.find()
+        socket.emit("todoResponse",{
+            message : "getting all details",
+            data : todoList
+        })
+
+
     }
 }
